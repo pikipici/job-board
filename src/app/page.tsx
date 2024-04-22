33a36184be987dbc0051ce9dc0232d684a4ee549
pikipici <fikriramaaulia@gmail.com>
2024-04-22 19:31:00 +0700

    menambahkan sidebar dengan fungsi filter search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,39 @@
-import JobListItem from "@/components/JobListItem";
-import prisma from "@/lib/prisma";
+import JobFilterSidebar from "@/components/JobFilterSidebar";
+import JobResults from "@/components/JobResults";
+import H1 from "@/components/ui/h1";
+import { JobFilterValues } from "@/lib/validation";
 
-export default async function Home() {
-  const jobs = await prisma.job.findMany({
-    where: {
-      approved: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+interface PageProps {
+  searchParams: {
+    q?: string;
+    location?: string;
+    type?: string;
+    remote?: string;
+  };
+}
+
+export default async function Home({
+  searchParams: { q, location, type, remote },
+}: PageProps) {
+  const filterValues: JobFilterValues = {
+    q,
+    location,
+    type,
+    remote: remote === "true",
+  };
 
   return (
-    <main>
-      {jobs.map((job) => (
-        <JobListItem job={job} key={job.id} />
-      ))}
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-x-5 text-center">
+        <H1>Developer Jobs</H1>
+
+        <p className="text-muted-foreground">Find your dream job.</p>
+      </div>
+
+      <section className="flex flex-col md:flex-row gap-4">
+        <JobFilterSidebar />
+        <JobResults filterValues={filterValues} />
+      </section>
     </main>
   );
 }
---
+++ src/components/JobFilterSidebar.tsx
@@ -0,0 +1,83 @@
+import { jobTypes } from "@/lib/job-types";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import Select from "./ui/select";
+import Prisma from "@/lib/prisma";
+import { Button } from "./ui/button";
+import { jobFilterSchema } from "@/lib/validation";
+import { redirect } from "next/navigation";
+
+async function filterJobs(formData: FormData) {
+  "use server";
+
+  const values = Object.fromEntries(formData.entries());
+  const { q, location, type, remote } = jobFilterSchema.parse(values);
+
+  const searchParams = new URLSearchParams({
+    ...(q && { q: q.trim() }),
+    ...(type && { type }),
+    ...(location && { location }),
+    ...(remote && { remote: "true" }),
+  });
+
+  redirect(`/?${searchParams.toString()}`);
+}
+
+export default async function JobFilterSidebar() {
+  const distinclocation = (await Prisma.job
+    .findMany({
+      where: { approved: true },
+      select: { location: true },
+      distinct: ["location"],
+    })
+    .then((locations) =>
+      locations.map(({ location }) => location).filter(Boolean)
+    )) as string[];
+
+  return (
+    <aside className="md:w-[260px] p-4 sticky top-0 h-fit bg-background border rounded-lg">
+      <form action={filterJobs}>
+        <div className="space-y-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="q">Search</Label>
+            <Input id="q" name="q" placeholder="Title, company, etc." />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="type">Type</label>
+            <Select name="type" id="type">
+              <option value="">All Types</option>
+              {jobTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="location">Location</label>
+            <Select id="location" name="location">
+              <option value="">All Locations</option>
+              {distinclocation.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              id="remote"
+              name="remote"
+              type="checkbox"
+              className="scale-125 accent-black"
+            />
+            <Label htmlFor="remote">Remote Jobs</Label>
+          </div>
+          <Button type="submit" className="w-full">
+            Filter Jobs
+          </Button>
+        </div>
+      </form>
+    </aside>
+  );
+}
